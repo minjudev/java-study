d37b59302621b015d146942360eff1576628f1e1
minjulee97 <aamj0226@gmail.com>
2020-12-24 23:43:54 +0900

    initial commit

---
+++ JavaProject/src/ex1/io/Program.java
@@ -0,0 +1,16 @@
+package ex1.io;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// 3이라는 숫자를 출력 버퍼에 채움
+		System.out.write(3);
+
+		System.out.write('A'+0);
+		System.out.write('A'+1);
+		System.out.write('A'+2);
+		System.out.write('A'+3);
+		System.out.flush();	
+	}
+
+}
