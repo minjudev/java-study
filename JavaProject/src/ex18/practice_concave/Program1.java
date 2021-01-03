377830c5fb3321d4047ed591bab409d9b09c8fa0
minjulee97 <aamj0226@gmail.com>
2021-01-03 22:36:07 +0900

    add: for문 오목 예제 추가

---
+++ JavaProject/src/ex18/practice_concave/Program1.java
@@ -0,0 +1,15 @@
+package ex18.practice_concave;
+
+public class Program1 {
+	public static void main(String[] args) {
+		
+		for(int y=1; y<=10; y++) {
+			for(int x=1; x<=10; x++) 
+				System.out.printf("%c", '¦«');
+			
+		System.out.println();
+		}
+		
+	}
+
+}
