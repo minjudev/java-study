1cc50af559d99217c5fb7a1868f70715bccee4de
minjulee97 <aamj0226@gmail.com>
2021-01-04 20:55:14 +0900

    add: for문 오목 돌 놓기 예제 추가

--- JavaProject/src/ex18/practice_concave/Program1.java
+++ JavaProject/src/ex18/practice_concave/Program1.java
@@ -5,8 +5,28 @@ public class Program1 {
 		
 		for(int y=1; y<=10; y++) {
 			for(int x=1; x<=10; x++) 
-				System.out.printf("%c", '┼');
-			
+				// 더 한정된 범위일수록 if문 상단에 두기
+				if(x==1 && y==1)
+					System.out.printf("%c", '┌');
+				else if(x==10 && y==1) 
+					System.out.printf("%c", '┐');
+				else if(x==1 && y==10) 
+					System.out.printf("%c", '└');
+				else if(x==10 && y==10) 
+					System.out.printf("%c", '┘');
+				else if(x==4 && y==3) 
+					System.out.printf("%c", '●');
+				else if(y==1)
+					System.out.printf("%c", '┬');
+				else if(y==10)
+					System.out.printf("%c", '┴');
+				else if(x==1)
+					System.out.printf("%c", '├');
+				else if(x==10)
+					System.out.printf("%c", '┤');
+				else 
+					System.out.printf("%c", '┼');
+		
 		System.out.println();
 		}
 		
