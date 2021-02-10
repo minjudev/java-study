129bf5082242faef3b29a4e1f5e4d58c8ac7b184
unknown <aamj0226@gmail.com>
2021-02-10 10:23:42 +0900

    add: 객체지향 프로그래밍 - Override 메소드

--- OOPJavaProject/src/NewlecExam.java
+++ OOPJavaProject/src/NewlecExam.java
@@ -1,6 +1,6 @@
 import part3.ex04.UI코드분리하기.Exam;
 
-public class NewlecExam extends Exam{
+public class NewlecExam extends Exam {
 
 	private int com;
 
@@ -12,4 +12,15 @@ public class NewlecExam extends Exam{
 		this.com = com;
 	}
 	
+	 @Override
+	public int total() {
+	
+		 return super.total()+com;
+	}
+	 
+	 @Override
+	public float avg() {
+	
+		 return total()/4.0f;
+	}
 }
