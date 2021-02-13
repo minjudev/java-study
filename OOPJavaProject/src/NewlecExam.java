2cb5b79fad8ade8ea2695c711a990072b59bf4d9
unknown <aamj0226@gmail.com>
2021-02-13 23:15:20 +0900

    add: 객체지향 프로그래밍 - 자식 클래스의 객체 초기화

--- OOPJavaProject/src/NewlecExam.java
+++ OOPJavaProject/src/NewlecExam.java
@@ -4,6 +4,17 @@ public class NewlecExam extends Exam {
 
 	private int com;
 
+	public NewlecExam() {
+		this(0, 0, 0, 0);
+	}
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		
+		// 부모 생성자를 호출해서 kor, eng, math를 세팅하게 함
+		super(kor, eng, math);
+		this.com = com;
+	}
+	
 	public int getCom() {
 		return com;
 	}
--- OOPJavaProject/src/Program.java
+++ OOPJavaProject/src/Program.java
@@ -3,11 +3,11 @@ import part3.ex04.UI코드분리하기.Exam;
 public class Program {
 	
 	public static void main(String[] args) {
-		NewlecExam exam = new NewlecExam();
-		exam.setKor(10);
+		NewlecExam exam = new NewlecExam(1, 1, 1, 1);
+		/*exam.setKor(10);
 		exam.setEng(10);
 		exam.setMath(10);
-		exam.setCom(10);
+		exam.setCom(10);*/
 		
 		System.out.println(exam.total());
 		System.out.println(exam.avg());
